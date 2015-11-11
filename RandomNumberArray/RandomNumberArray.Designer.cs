f3ea9ae448530815dfad5334cd4bebdb81ad74e6
RyanStLouis <ryan.stlouis@ocsbstudent.ca>
2015-11-11 14:35:01 -0500

    array

---
+++ RandomNumberArray/RandomNumberArray.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace RandomNumberArray
+{
+    partial class frmRandomNumberArray
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstRandomBox = new System.Windows.Forms.ListBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.lblQuestion = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lstRandomBox
+            // 
+            this.lstRandomBox.FormattingEnabled = true;
+            this.lstRandomBox.Location = new System.Drawing.Point(82, 75);
+            this.lstRandomBox.Name = "lstRandomBox";
+            this.lstRandomBox.Size = new System.Drawing.Size(120, 95);
+            this.lstRandomBox.TabIndex = 0;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalculate.Location = new System.Drawing.Point(82, 196);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(120, 32);
+            this.btnCalculate.TabIndex = 1;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // lblQuestion
+            // 
+            this.lblQuestion.AutoSize = true;
+            this.lblQuestion.Location = new System.Drawing.Point(89, 43);
+            this.lblQuestion.Name = "lblQuestion";
+            this.lblQuestion.Size = new System.Drawing.Size(0, 13);
+            this.lblQuestion.TabIndex = 2;
+            // 
+            // frmRandomNumberArray
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.lblQuestion);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.lstRandomBox);
+            this.Name = "frmRandomNumberArray";
+            this.Text = "Random Number Array";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstRandomBox;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label lblQuestion;
+    }
+}
+
---
+++ RandomNumberArray/RandomNumberArray.cs
@@ -0,0 +1,59 @@
+﻿/*
+ * Created by: Ryan St. Louis
+ * Created on: 11-11-2015
+ * Created for: ICS3U
+ * Daily Assignment – Unit#5-01
+ * This program displays an example of a array
+*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RandomNumberArray
+{
+    public partial class frmRandomNumberArray : Form
+    {
+        // global variables
+        int[] arrayOfNumbers = new int[10];
+
+        public frmRandomNumberArray()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            this.lstRandomBox.Items.Clear();
+
+
+            // Variables
+            int loopCounter = 0;
+            int average = 0;
+            int listAdder;
+            int randomNumbers;
+
+            Random rnd = new Random();
+
+            while (loopCounter < 10)
+            {
+
+                randomNumbers = rnd.Next(1, 11);
+                arrayOfNumbers[loopCounter] = randomNumbers;
+                listAdder = arrayOfNumbers[loopCounter];
+                this.lstRandomBox.Items.Add(listAdder);
+                average = average + arrayOfNumbers[loopCounter];
+                loopCounter++;
+            }
+            average = average / 10;
+            this.lblQuestion.Text = "This is the average: " + average;
+
+        }
+    }
+}
